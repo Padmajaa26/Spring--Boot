983b2f22182b7618bc906723a8b5df77ff8bf4dd
Padmajaa26 <Padmajaa26@example.com>
2024-09-05 22:15:20 +0530

    Sprintboot Project

---
+++ placementservice/src/main/java/com/tns/placementservice/PlacementRepository.java
@@ -0,0 +1,6 @@
+package com.tns.placementservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlacementRepository extends JpaRepository<Placement, Long> {
+}
